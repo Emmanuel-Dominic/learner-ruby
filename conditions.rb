fb570d6fa786ca3274e92d2b5b0ab042bb4896f5
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2024-12-01 00:24:36 +0300

    implements case statement

--- conditions.rb
+++ conditions.rb
@@ -22,3 +22,32 @@ elsif is_male or is_tall
 else
     puts "He isnt a tall male"
 end
+
+
+
+def get_day_name(day)
+    day_name = ""
+    case day
+    when "mon"
+        day_name = "Monday"
+    when "tue"
+        day_name = "Tuesday"
+    when "wed"
+        day_name = "Wednesday"
+    when "thur"
+        day_name = "Thurday"
+    when "fri"
+        day_name = "Friday"
+    when "sat"
+        day_name = "Saturday"
+    when "sun"
+        day_name = "Sunday"
+    else
+        day_name = "Invalid value entered"
+    end
+
+    return day_name
+end
+
+puts get_day_name("mon")
+puts get_day_name("dom")
