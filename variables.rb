9f586549915db2da36cb7a7305ec8bfb7618fe57
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2024-11-30 16:55:53 +0300

    implements variables

---
+++ variables.rb
@@ -0,0 +1,8 @@
+character_name = "Dominic"
+character_age = "29"
+
+puts("There once was a man name " + character_name)
+puts("he was " + character_age + " years old.")
+character_name = "Emmanuel"
+puts("He really lied the name " + character_name)
+puts("but didn't like being " + character_age)
