2dadb4004bd747b5541801b1b43c63f257389d11
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2024-12-01 02:35:36 +0300

    implements writing files

--- files.rb
+++ files.rb
@@ -18,3 +18,17 @@ end
 file = File.open("employees.txt", "r")
 file.read
 file.close()
+
+
+file = File.open("employees.txt", "a")
+file.write("\nOscar, Accounting")
+file.close()
+
+file = File.open("index.html", "w") # this writes to the file, replacing the existing content
+file.write("<h1>Oscar, Accounting<h1>")
+file.close()
+
+# "r" # Read-only mode for a file.
+# "r+" # Read-Write mode for a file.
+# "w"  # Write-only mode for a file.
+# "w+" # Read-Write mode for a file.
