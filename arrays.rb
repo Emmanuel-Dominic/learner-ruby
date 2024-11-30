00a979bb43a2a097d4b893cf90bc51eac031d950
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2024-11-30 18:41:53 +0300

    implements arrays

---
+++ arrays.rb
@@ -0,0 +1,16 @@
+friends = Array["Chris", "Arthur", "Zack", "Harriet"]
+puts friends
+puts friends[1]
+puts friends[-3]
+puts friends[1, 2]
+puts friends[0, 2]
+
+teams = Array.new
+teams[0] = "Dominic"
+teams[1] = "Jean"
+puts teams
+puts teams.length
+puts teams.reverse()
+puts teams.include? "Dominic"
+puts friends.include? "Dominic"
+puts friends.sort()
