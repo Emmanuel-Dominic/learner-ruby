8e2addcb872288084bd9eb6f2274b46ded64c265
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2024-12-01 00:57:14 +0300

    implements for loop

--- loops.rb
+++ loops.rb
@@ -3,3 +3,22 @@ while index <= 5
     puts index
     index += 1
 end
+
+
+friends = ["Arthur", "Chris", "Harriet", "Zack", "Derrick"]
+
+for friend in friends
+    puts friend
+end
+
+friends.each do |friend|
+    puts friend
+end
+
+for xindex in 0..5
+    puts xindex
+end
+
+6.times do |yindex|
+    puts yindex
+end
