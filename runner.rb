42d27aceb7eeabf431e50c7195fc55946c80f2cc
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2024-11-30 18:27:37 +0300

    implements Mad Libs Game

--- runner.rb
+++ runner.rb
@@ -1,6 +1,20 @@
 puts "Enter Your Name: "
 name = gets
 puts("Hello #{name}, you're cool")
+puts "Enter Your Name: "
 name = gets.chomp() # eliminate the new line created due to the enter key
+puts "Enter Your age: "
 age = gets.chomp()
 puts("Hello #{name}, you're #{age}")
+
+
+puts "Enter a color: "
+color = gets.chomp()
+puts "Enter a plural noun: "
+plural_noun = gets.chomp()
+puts "Enter a celebrity: "
+celebrity = gets.chomp()
+
+puts("Roses are #{color}")
+puts("#{plural_noun} are blue")
+puts("I love #{celebrity}")
